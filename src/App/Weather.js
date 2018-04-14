ec548dfd1bd9d9a38cccfed560554d2ddd883765
Seth Watson <seth.watson1@gmail.com>
2018-04-14 12:50:47 -0700

    added input handler

--- src/App/Weather.js
+++ src/App/Weather.js
@@ -2,11 +2,27 @@ import React from 'react';
 
 
 class Weather extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            location: ""
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange() {
+
+    }
+
     render() {
         return (
             <main>
                 <form>
-                    <input type="text" />
+                    <input onChange={this.handleChange}
+                        type="text"
+                        name="location"
+                        value={this.state.location}
+                    />
                     <button>Get Weather</button>
                 </form>
             </main>
